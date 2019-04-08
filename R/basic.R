54fb84c7729643553c3542ccacea8eb91161d034
Zebulun Arendsee <arendsee@iastate.edu>
2019-04-08 09:59:43 -0500

    Make plot_basic work for all segments

--- R/basic.R
+++ R/basic.R
@@ -62,48 +62,89 @@ load_current <- function(){
 #' Make a basic barplot of the data
 #'
 #' @param d data.frame swine surveillance data (i.e., output or \code{load_current})
+#' @param segment The influenze segment that will be plotted [H1,H3,N1,N3,PB2,PB1,PA,NP,M,NS]
+#' @param byMonth Should be plot be by month (if FALSE, then it will be by day)
 #' @export
 #' @return ggplot object
-plot_basic <- function(d){
+plot_basic <- function(d, segment="H1", byMonth=TRUE){
   # ===== Basic Counting and plotting
   # ==== Colors and Order of names
   # Stacked bar chart for phylocluster by month
-  H1_palette <- c("alpha" = "#543005", "beta" = "#8c510a", "gamma" = "#80cdc1", "gamma2" = "#35978f",
-                  "delta1"="#bf812d","delta1a"="#dfc27d","delta1b"="#f6e8c3","delta2"="#c7eae5",
-                  "pandemic"="#01665e","gamma2-beta-like"="#60b0f4","No_data"="#EDF8B1")
-  H3_order <- c("2010.1","2010.2","other-human","I","II","III","IV","IV-A","IV-B","IV-C","IV-D","IV-E","IV-F","No_data")
-  H3_palette <- c("2010.1"="#999999","2010.2"="#996633","other-human"="#F90005","I"="#542788","II"="#d73027","III"="#2166AC",
-                  "IV"="#E69F00","IV-A"="#56B4E9","IV-B"="#009E73","IV-C"="#F0E442","IV-D"="#0072B2","IV-E"="#D55E00","IV-F"="#CC79A7",
-                  "No_data"="#EDF8B1")
-  N1_palette <- c("Classical"="#FEB24C","Pandemic"="#F03B20","Human_seasonal"="#BD2D2B","MN99"="#91BFDB","No_data"="#EDF881")
-  N1_order <- c("Classical","Pandemic","Human_seasonal","MN99","No_data")
-  N2_palette <- c("1998"="#7FCDBB","98"="#7FCDBB", "98A" ="#78c8b4", "98A1"="#1b7837", "98A2"="#9970ab", "98B"="#82d2be", "98B1"="#5aae61","98B2"="#762a83",
-                  "2002"="#2C7FB8","02"="#2C7FB8", "02A" ="#2878b4", "02A1"="#d6604d", "02A2"="#b2182b", "02B"="#3282be", "02B1"="#d1e5f0","02B2"="#fddbc7",
-                  "Human-like"="#1F38FF", "Human_N2"="#969696", "Human-to-swine_2016"="#1F38FF","Human-like_2010.2"="#996633","2016"="#996633", 
-                  "TX98"="#C7EAE5", "No_data"="#EDF8B1")
 
-  internalgene_palette <- c("TRIG"="#C2A5CF","PDM"="#762A83","unknown"="#800000","TX98"="#C7EAE5")
+  # NOTE: currently the subtype palette is not used
   subtype_palette <- c("#d7191c","#fdae61","#ffffbf","#abd9e9","#2C7FB8","#e0f3f8","#2c7bb6","#2c7bb6","#2c7bb6","#2c7bb6","#2c7bb6")
+  internalgene_palette <- c("TRIG"="#C2A5CF","PDM"="#762A83","unknown"="#800000","TX98"="#C7EAE5")
+  segment_palette <- switch(
+    segment,
+    H1 = c("alpha"="#543005", "beta"="#8c510a", "gamma"="#80cdc1",
+           "gamma2"="#35978f", "delta1"="#bf812d", "delta1a"="#dfc27d",
+           "delta1b"="#f6e8c3", "delta2"="#c7eae5", "pandemic"="#01665e",
+           "gamma2-beta-like"="#60b0f4", "No_data"="#EDF8B1"),
+    H3 = c("2010.1"="#999999", "2010.2"="#996633", "other-human"="#F90005",
+           "I"="#542788", "II"="#d73027", "III"="#2166AC", "IV"="#E69F00",
+           "IV-A"="#56B4E9", "IV-B"="#009E73", "IV-C"="#F0E442",
+           "IV-D"="#0072B2", "IV-E"="#D55E00", "IV-F"="#CC79A7",
+           "No_data"="#EDF8B1"),
+    N1 = c("Classical"="#FEB24C", "Pandemic"="#F03B20",
+           "Human_seasonal"="#BD2D2B", "MN99"="#91BFDB", "No_data"="#EDF881"),
+    N2 = c("1998"="#7FCDBB", "98"="#7FCDBB", "98A" ="#78c8b4",
+           "98A1"="#1b7837", "98A2"="#9970ab", "98B"="#82d2be",
+           "98B1"="#5aae61", "98B2"="#762a83", "2002"="#2C7FB8",
+           "02"="#2C7FB8", "02A" ="#2878b4", "02A1"="#d6604d",
+           "02A2"="#b2182b", "02B"="#3282be", "02B1"="#d1e5f0",
+           "02B2"="#fddbc7", "Human-like"="#1F38FF", "Human_N2"="#969696",
+           "Human-to-swine_2016"="#1F38FF", "Human-like_2010.2"="#996633",
+           "2016"="#996633", "TX98"="#C7EAE5", "No_data"="#EDF8B1"),
+    PB2 = internalgene_palette,
+    PB1 = internalgene_palette,
+    PA = internalgene_palette,
+    NP = internalgene_palette,
+    M = internalgene_palette,
+    NS = internalgene_palette
+  )
 
-  d$ByMonth=d$Date
-  lubridate::day(d$ByMonth)=1
+  # Die if the segment is not correctly named
+  stopifnot(segment %in% c("H1", "H3", "N1", "N2", "PB2", "PB1", "PA", "NP", "M", "NS"))
+  # The segment must be a column name in the data
+  stopifnot(segment %in% names(d))
+  # The input data must have a Date column
+  stopifnot("Date" %in% names(d))
 
-  # Prepping ggplot input data (Current Version)
-  summary.data = d %>%
-    subset(!is.na(H1)) %>%{
-      .$H1 = gsub(",.*","",.$H1)
-      .
-    } %>%
-    dplyr::group_by(., ByMonth, H1) %>% # Group by Month and H1 clade (extend this to any segment H3, N1, N2, etc)
+  if(byMonth){
+    lubridate::day(d$Date) <- 1
+  }
+
+  # Get user-provided segment
+  m <- d[, c("Date", segment)]
+  names(m)[2] <- "Segment"
+  m <- subset(m, !is.na(Segment))
+  # e.g., ("unknown,TRIG" -> "unknown")
+  m$Segment <- gsub(",.*", "", m$Segment)
+
+  # --- segment-specific prep -----------------------
+  if(segment == "H1"){
+    m$Segment <- factor(m$Segment)
+  }
+  else if(segment == "H3"){
+    H3_order <- c("2010.1","2010.2","other-human","I","II","III","IV","IV-A","IV-B","IV-C","IV-D","IV-E","IV-F","No_data")
+    m$Segment <- factor(m$Segment, ordered=TRUE, levels=H3_order)
+  }
+  else if(segment == "N1"){
+    N1_order <- c("Classical","Pandemic","Human_seasonal","MN99","No_data")
+    m$Segment <- factor(m$Segment, ordered=TRUE, levels=N1_order)
+  }
+  # -------------------------------------------------
+
+  summary.data <- m %>%
+    dplyr::group_by(Date, Segment) %>%
     dplyr::summarise(n=dplyr::n(),
                      tot=nrow(.),
-                     per = round(dplyr::n() / nrow(.)*100, digits = 2))  # Calculate summary statistics
+                     per=round(dplyr::n() / nrow(.)*100, digits = 2))  # Calculate summary statistics
 
-  # example barchart
-  ggplot2::ggplot(summary.data, ggplot2::aes(x=ByMonth, y=n, fill=H1)) +
-    ggplot2::geom_bar(stat="identity",position = "stack") +
+  ggplot2::ggplot(summary.data, ggplot2::aes(x=Date, y=n, fill=Segment)) +
+    ggplot2::geom_bar(stat="identity", position="stack") +
     ggplot2::theme_bw() +
-    ggplot2::scale_fill_manual(values = H1_palette) +
+    ggplot2::scale_fill_manual(values=segment_palette) +
     ggplot2::theme(
       axis.text.x     = ggplot2::element_text(angle=90, size=10, vjust=0.5),
       legend.title    = ggplot2::element_blank(),
