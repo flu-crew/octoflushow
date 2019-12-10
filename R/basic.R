f3f6c5f70ae3613fd80c56362aadb62daf4dc68e
Zebulun Arendsee <arendsee@iastate.edu>
2019-12-10 12:31:36 -0600

    Updated plot_basic

--- R/basic.R
+++ R/basic.R
@@ -124,9 +124,11 @@ order_data_factors <- function(d, config){
 #' @param floorDateBy An argument that is passed to
 #' \code{lubridate::floor_date} specifying how to group dates (e.g., "month",
 #' "week", "3 months", "year")
+#' @param dateFormat A formatting string for the x-axis date labels (e.g. "%Y" or "%m-%Y")
+#' @param dateBreaks Date to break by (e.g., "years", or "months")
 #' @export
 #' @return ggplot object
-plot_basic <- function(d, segment="H1", floorDateBy="month"){
+plot_basic <- function(d, segment="H1", floorDateBy="month", dateFormat="%Y", dateBreaks="years", xlim=NULL){
   # ===== Basic Counting and plotting
   # ==== Colors and Order of names
   # Stacked bar chart for phylocluster by month
@@ -143,7 +145,11 @@ plot_basic <- function(d, segment="H1", floorDateBy="month"){
 
   d <- order_data_factors(clean_data(d), config)
 
-  d$Date <- lubridate::floor_date(d$Date, floorDateBy)
+  d$Date <- lubridate::as_datetime(lubridate::floor_date(d$Date, floorDateBy))
+
+  if (is.null(xlim)){
+    xlim <- lubridate::as_datetime(c(min(d$Date), max(d$Date)))
+  }
 
   # Get user-provided segment
   m <- d[, c("Date", segment)]
@@ -158,38 +164,29 @@ plot_basic <- function(d, segment="H1", floorDateBy="month"){
 
   summary.data$Date <- as.POSIXct(summary.data$Date)
 
-  unscaled <- ggplot2::ggplot(summary.data, ggplot2::aes(x=Date, y=n, fill=Segment)) +
-    ggplot2::geom_bar(stat="identity", position="stack") +
-    ggplot2::theme_bw() +
-    ggplot2::ggtitle(paste(segment, "phylogenetic-clades by", floorDateBy)) +
-    ggplot2::scale_fill_manual(values=segment_palette) +
-    # ggplot2::scale_x_datetime(labels = scales::date_format("%m-%Y"), breaks = scales::date_breaks("months")) +
-    ggplot2::scale_x_datetime(labels = scales::date_format("%Y"), breaks = scales::date_breaks("years")) +
-    ggplot2::theme(
-      axis.text.x     = ggplot2::element_text(angle=90, size=10, vjust=0.5),
-      legend.title    = ggplot2::element_blank(),
-      legend.position = "none", # no legend since it will be the same as the scaled fig
-      strip.text      = ggplot2::element_text(size=12),
-    ) +
-    ggplot2::labs(x="", y="Number of Swine Isolates") # also fix the x and y axis labels...
+  plotit <- function(position, ylab){
+    ggplot2::ggplot(summary.data, ggplot2::aes(x=Date, y=n, fill=Segment)) +
+      ggplot2::geom_bar(stat="identity", position=position) +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle(paste(segment, "phylogenetic-clades by", floorDateBy)) +
+      ggplot2::scale_fill_manual(values=segment_palette) +
+      ggplot2::scale_x_datetime(labels = scales::date_format(dateFormat), breaks = scales::date_breaks(dateBreaks)) +
+      ggplot2::coord_cartesian(xlim=xlim) +
+      ggplot2::theme(
+        axis.text.x     = ggplot2::element_text(angle=90, size=10, vjust=0.5),
+        legend.title    = ggplot2::element_blank(),
+        legend.position = "bottom",
+        strip.text      = ggplot2::element_text(size=12),
+      ) +
+      ggplot2::labs(x="", y=ylab)
+  }
 
-  scaled <- ggplot2::ggplot(summary.data, ggplot2::aes(x=Date, y=n, fill=Segment)) +
-    ggplot2::geom_bar(stat="identity", position="fill") +    # duplicate, and change "stack" to "fill"
-    ggplot2::theme_bw() +
-    ggplot2::ggtitle(paste(segment, "phylogenetic-clades by", floorDateBy)) +
-    ggplot2::scale_x_datetime(labels = scales::date_format("%Y"), breaks = scales::date_breaks("years")) +
-    ggplot2::scale_fill_manual(values=segment_palette)+
-    ggplot2::theme(
-      axis.text.x     = ggplot2::element_text(angle=90, size=10,vjust=0.5),
-      legend.title    = ggplot2::element_blank(),
-      legend.position = "bottom",
-      strip.text      = ggplot2::element_text(size=12)
-    ) +
-    ggplot2::scale_y_continuous(breaks = c(0,0.25,0.5,0.75,1), labels = c("0%","25%","50%","75%","100%")) +
-    ggplot2::labs(x="", y="Swine Isolates by %") # also fix the x and y axis labels...
+  unscaled <- plotit("stack", ylab="Number of Swine Isolates")
+  scaled <- plotit("fill", ylab="Swine Isolates by %")
 
   legend <- cowplot::get_legend(scaled)
-  scaled <- scaled + ggplot2::theme(legend.position = "none")
+  unscaled <- unscaled + ggplot2::theme(legend.position = "none")
+  scaled   <- scaled   + ggplot2::theme(legend.position = "none")
 
   cowplot::plot_grid(unscaled, scaled, legend, rel_heights=c(1,1,0.3), ncol=1, labels=NULL)
 }
