d851d9f30b120b37d026c2129bc7b629b5a16ce6
j23414 <jenchang@iastate.edu>
2020-02-24 15:18:43 -0600

    Landing page
    * fillin text for landing page
    [] add octoFLU logo

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinyBS)
 library(wilbur)
+#library(png)
 
 infile <- system.file("app-data", "A0_Master_List.xlsx", package="wilbur")
 sheet <- "Data"
@@ -23,15 +24,30 @@ resize_on_change <- function(elementID){
   tags$script(js)
 }
 
-shinyUI(navbarPage("Swine Surveillance App",
+#logofile <- system.file("app-data", "logo.png", package="wilbur")
+
+shinyUI(navbarPage("octoFLU show",
   tabPanel(
     "Landing page",
     fluidPage(
-      h1("THIS IS HOW YOU DO THING 1")
-      h1("THIS IS HOW YOU DO THING 2")
-      h1("THIS IS HOW YOU DO THING 3")
-      h1("EXAMPLE FIGURE!!!!!!!")
-      h1("CLICK HERE TO SEE A QUARTERLY REPORT !!!! :)")
+      h1("Influenza A Virus in Swine Surveillence"),
+      p("The purpose of this site is to summarize the phylogenetic clade diversity of Influenza A Virus strains submitted to the passive surveillence program. octoFLU show provides a graphical dashboard of these changes and is maintained by Flu-Crew."),
+#      tags$img(src = "https://github.com/flu-crew/octoFLU/blob/master/img/octoFLU_revised_V3-01.png", width = "100px", height = "100px"),
+#      tags$img(src = logofile, width = "100px", height = "100px"),
+      h2("Data"),
+      p("The Data tab contains a table of all influenza A virus in swine isolates with A0 barcodes. Use this tab to filter and focus on subsets of the data. Filters will affect which isolates are included in the plots on the next three tabs."),
+      p("Common filters include:"),
+      tags$ul(
+      tags$li("Subset to any isolates submitted in the last two years (select date and drag the bar)"),
+      tags$li("Focus on isolates collected from a particular state (type in two letter state code in state column)"),
+      tags$li("Determine which isolates have a particular clade (type the clade name in the clade column) and regional information (click on Clades-by-state tab)")
+      ),
+      h2("Clades by time"),
+      p("Temporal patterns of clades represented in a barchart. Select a segment (H1, H3, PB2,...NS) of interest and the phylogenetic clade of that segment is shown in stacked barchart by time."),
+      h2("Clades by state"),
+      p("Regional patterns of clades represented in a ggmap. Select a segment (H1, H3, PB2, ... NS) of interest and the map is faceted by phylogenetic clade with isolate counts."),
+      h2("Clades by heatmap"),
+      p("Hemagglutinin (HA) and neuraminidase (NA) patterns represented in a heatmap. HA clades are represented by rows while NA clades are represented by columns. Heatmap can be used to determine most common HA-NA clade pairing for a particular data filter.")
     )
   ),
   tabPanel(
