367c52ac5f9611d30826b627a49a3f315a8b043f
Zebulun Arendsee <zbwrnz@gmail.com>
2021-12-30 13:48:49 -0600

    Create html landing page and add citation

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -31,36 +31,7 @@ shinyUI(navbarPage("octoFLUshow",
   tabPanel(
     "Landing page",
     fluidPage(
-      h1("USDA Influenza A Virus in Swine Surveillence"),
-      p("The purpose of this site is to summarize the genetic diversity of influenza A virus in swine submitted to the national USDA influenza A virus in swine passive surveillence program. octoFLU Show provides a graphical dashboard of these changes and is maintained by the Flu-Crew at the National Animal Disease Center, USDA-ARS."),
-#      tags$img(src = "https://github.com/flu-crew/octoFLU/blob/master/img/octoFLU_revised_V3-01.png", width = "100px", height = "100px"),
-#      tags$img(src = logofile, width = "100px", height = "100px"),
-      h2("Support"),
-      p("octoFLUshow is in developlemt. If you need to report a problem, or have a suggested enhancement, please create an issue at https://github.com/flu-crew/octoflushow or contact Tavis Anderson or Zebulun Arendsee."), 
-      h3("How to cite"),
-      p("If you use data provided by octoFLU Show in your work, please credit in the following format:"),
-#      p("octoFLU show, https://flu-crew.org/octoflushow/, (data retrieved <?php echo (new DateTime())->format('d M, Y');?>)"),
-      p("octoFLU Show, https://flu-crew.org/octoflushow/, (data retrieved on dd/MM/YYYY)."),
-      p("Citation is subject to change pending publication."),
-      h2("Data"),
-      p("Use the Data tab to select the surveillance data you would like to graph."),
-      p("The Data tab contains a table of all influenza A virus in swine isolates with A0 barcodes. Use this tab to filter and focus on subsets of the data. Filters will affect which isolates are included in the plots on the next three tabs."),
-      p("Common filters include:"),
-      tags$ul(
-      tags$li("Subset to any isolates submitted in the last two years (select date and drag the bar)"),
-      tags$li("Focus on isolates collected from a particular state (type in two letter state code in state column)"),
-      tags$li("Determine which isolates have a particular clade (type the clade name in the clade column) and regional information (click on Clades-by-state tab)")
-      ),
-      h2("Clades by time"),
-      p("Temporal patterns of clades represented in a barchart. Select a segment (H1, H3, PB2,...NS) of interest and the phylogenetic clade of that segment is shown in stacked barchart by time."),
-      h2("Clades by state"),
-      p("Regional patterns of clades represented in a ggmap. Select a segment (H1, H3, PB2, ... NS) of interest and the map is faceted by phylogenetic clade with isolate counts."),
-      h2("Clade heatmap"),
-      p("Hemagglutinin (HA) and neuraminidase (NA) patterns represented in a heatmap. HA clades are represented by rows while NA clades are represented by columns. Heatmap can be used to determine most common HA-NA clade pairing for a particular data filter."),
-  h2("Constellation heatmap"),
-  p("Internal gene constellation patterns of subtypes where T=TRIG, P=Pandemic, and V=LAIV-related. Strains with mixed subtypes or mixed internal constellations were filtered out. The heatmap can be used to determine the most common Gene Constellation from the left Totals column."),
-  h3("Acknowledgements:"),
-  p("We were supported by USDA-ARS and USDA-APHIS. We are grateful to the pork producers, swine veterinarians, and laboratories for participating in the USDA Influenza Virus Surveillance System for swine. This research used resources provided by the SCINet project of the USDA Agricultural Research Service, ARS project number 0500-00093-001-00-D.")
+      includeHTML("landing-page.html")
     )
   ),
 
